d3bd97b6098f6de101832847ce95a045b00c574c
Étienne Bouchard <etiennebouchard7@gmail.com>
2022-01-24 11:01:50 -0500

    Websocket authentication

--- lib/src/routers/websocket.dart
+++ lib/src/routers/websocket.dart
@@ -4,12 +4,29 @@ import 'package:alfred/src/type_handlers/websocket_type_handler.dart';
 import 'package:apollocode_dart_utilities/src/services/websocket.dart';
 
 class WebsocketRouter {
-  WebsocketRouter(NestedRoute app) {
+  WebsocketRouter(NestedRoute app, { bool useAuth = true, Future<bool> Function(String token)? authValidationFunction }) {
     app.get('/', (req, res) {
       return WebSocketSession(
-        onOpen: (ws) {
+        onOpen: (ws) async {
+          
+          if (useAuth) {
+            var authorization = req.uri.queryParameters['authorization'] ?? "";
+            if (authValidationFunction == null) {
+              throw Error();
+            }
+
+            var tokenIsValid = await authValidationFunction(authorization);
+
+            if (!tokenIsValid) {
+              print("A websocket connection failed: authentication failed.");
+              ws.close(493, "Authentication failed.");
+              return;
+            }
+          }
+          
           WebsocketService.users.add(ws);
           print("USER JOINED");
+          
           WebsocketService.users
               .where((user) => user != ws)
               .forEach((user) => user.send('A new user joined the chat.'));
