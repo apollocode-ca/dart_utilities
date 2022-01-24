35120564bafb26cd67e3204703ff360bdd1c827e
Étienne Bouchard <etiennebouchard7@gmail.com>
2022-01-24 09:53:20 -0500

    First Commit

---
+++ lib/apollocode_dart_utilities.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library apollocode_dart_utilities;
+
+export 'src/apollocode_dart_utilities_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
