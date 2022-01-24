05e074a943058cd8e9eddd0e8927526ec0482d97
Étienne Bouchard <etiennebouchard7@gmail.com>
2022-01-24 11:13:27 -0500

    WS identifier

--- lib/src/routers/websocket.dart
+++ lib/src/routers/websocket.dart
@@ -1,43 +1,51 @@
 import 'package:alfred/alfred.dart';
 // ignore: implementation_imports
 import 'package:alfred/src/type_handlers/websocket_type_handler.dart';
+import 'package:apollocode_dart_utilities/src/models/ws_user.dart';
 import 'package:apollocode_dart_utilities/src/services/websocket.dart';
 
 class WebsocketRouter {
-  WebsocketRouter(NestedRoute app, { bool useAuth = true, Future<bool> Function(String token)? authValidationFunction }) {
+  WebsocketRouter(NestedRoute app,
+      {bool useAuth = true,
+      Future<bool> Function(String token)? authValidationFunction,
+      Future<String> Function(String token)? getIdentifier}) {
     app.get('/', (req, res) {
       return WebSocketSession(
-        onOpen: (ws) async {
-          
-          if (useAuth) {
-            var authorization = req.uri.queryParameters['authorization'] ?? "";
-            if (authValidationFunction == null) {
-              throw Error();
+          onOpen: (ws) async {
+            String? id;
+
+            if (useAuth) {
+              var authorization =
+                  req.uri.queryParameters['authorization'] ?? "";
+              if (authValidationFunction == null || getIdentifier == null) {
+                throw Error();
+              }
+
+              var tokenIsValid = await authValidationFunction(authorization);
+
+              if (!tokenIsValid) {
+                print("A websocket connection failed: authentication failed.");
+                ws.close(493, "Authentication failed.");
+                return;
+              }
+
+              id = await getIdentifier!(authorization);
             }
 
-            var tokenIsValid = await authValidationFunction(authorization);
+            WebsocketService.users.add(WsUser(ws, id!));
+            print("USER JOINED");
 
-            if (!tokenIsValid) {
-              print("A websocket connection failed: authentication failed.");
-              ws.close(493, "Authentication failed.");
-              return;
-            }
-          }
-          
-          WebsocketService.users.add(ws);
-          print("USER JOINED");
-          
-          WebsocketService.users
-              .where((user) => user != ws)
-              .forEach((user) => user.send('A new user joined the chat.'));
-        },
-        onClose: (ws) {
-          WebsocketService.users.remove(ws);
-          print("USER LEFT");
-          WebsocketService.users.forEach((user) => user.send('A user has left.'));
-        },
-        onMessage: WebsocketService.onMessage
-      );
+            WebsocketService.users
+                .where((user) => user.ws != ws)
+                .forEach((user) => user.ws.send('A new user joined the chat.'));
+          },
+          onClose: (ws) {
+            WebsocketService.users.removeWhere((element) => element.ws == ws);
+            print("USER LEFT");
+            WebsocketService.users
+                .forEach((user) => user.ws.send('A user has left.'));
+          },
+          onMessage: WebsocketService.onMessage);
     });
   }
 }
--- lib/src/services/websocket.dart
+++ lib/src/services/websocket.dart
@@ -1,11 +1,12 @@
 import 'dart:io';
 
 import 'package:apollocode_dart_utilities/src/models/ws_data.dart';
+import 'package:apollocode_dart_utilities/src/models/ws_user.dart';
 import 'package:eventify/eventify.dart';
 
 class WebsocketService {
   static EventEmitter emitter = EventEmitter();
-  static List<WebSocket> users = [];
+  static List<WsUser> users = [];
 
   static Future onMessage(ws, dynamic data) async {
     data = data as WSData;
